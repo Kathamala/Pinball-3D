94a08f4e0b76d0553711df60b23eaa5ef8d37291
Thiago Vinicius <thiagovao2000@hotmail.com>
2021-07-08 20:16:48 -0300

    Adiciona obstáculos, pontuação e tela de game over

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-
-    public GameObject gameOverScreen;
+    public int amountOfBalls = 3;
     // Start is called before the first frame update
+
+    [HideInInspector] public bool isOnGame = false;
+
+    [HideInInspector] public int points = 0;
     void Start()
     {
         
@@ -15,7 +18,9 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < 7){
+            isOnGame = true;
+        }
     }
 
     //Upon collision with another GameObject, this GameObject will reverse direction
@@ -23,8 +28,39 @@ public class Ball : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Respawn"))
         {
-            gameOverScreen.GetComponent<MeshRenderer>().enabled = true;
-            print("Morreu");
+            if(amountOfBalls > 0){
+                amountOfBalls--;
+            }
+
+            if(amountOfBalls != 0){
+                transform.position = new Vector3(14f, 1.1f, -12f);
+                isOnGame = false;
+            }
+        }
+
+    }
+
+     void OnCollisionEnter (Collision other) 
+    {
+        if (other.gameObject.CompareTag("Walls"))
+        {
+            points += 1;
+        }
+        if (other.gameObject.CompareTag("30Points"))
+        {
+        points += 30;
+        }
+        if (other.gameObject.CompareTag("20Points"))
+        {
+        points += 20;
+        }
+        if (other.gameObject.CompareTag("10Points"))
+        {
+            points += 10;
+        }
+        if (other.gameObject.CompareTag("2Points"))
+        {
+            points += 2;
         }
     }
 }
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject ball;
+    public int score = 0;
+    public GameOverScreen GameOverScreen;
+    public Text scoreText;
+    public GameObject door;
+    private bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ball != null){
+            score = ball.GetComponent<Ball>().points;
+            if(ball.GetComponent<Ball>().amountOfBalls == 0 && !gameOver){
+                Destroy(ball);
+                Destroy(scoreText);
+                gameOver = true;
+                GameOver();
+            }
+
+            if(ball.GetComponent<Ball>().isOnGame){
+                door.GetComponent<BoxCollider>().enabled = true;
+                door.GetComponent<MeshRenderer>().enabled = true;
+            } else{
+                door.GetComponent<BoxCollider>().enabled = false;
+                door.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+        
+        if(scoreText != null){
+            scoreText.text = "Points: " + score;
+        }
+    }
+
+    public void GameOver(){
+        GameOverScreen.Setup(score);
+    }
+}
