47fca30f29338811572dd49378fbefa935234625
Thiago Vinicius <thiagovao2000@hotmail.com>
2021-07-15 14:49:10 -0300

    Realiza ajustes no Canvas e no código do lançador da bola

--- Assets/Scripts/PlangerScript.cs
+++ Assets/Scripts/PlangerScript.cs
@@ -8,14 +8,15 @@ public class PlangerScript : MonoBehaviour
     float power;
     public float maxPower = 100f;
     public Slider powerSlider;
-    List<Rigidbody> ballList;
     bool ballReady;
+    public GameObject ball;
+    private Rigidbody ballRigidbody;
 
     void Start()
     {
         powerSlider.minValue = 0f;
         powerSlider.maxValue = maxPower;
-        ballList = new List<Rigidbody>();
+        ballRigidbody = ball.GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -28,9 +29,7 @@ public class PlangerScript : MonoBehaviour
 
         powerSlider.value = power;
 
-        if(ballList.Count > 0){
-            ballReady = true;
-
+        if(ballReady){
             if(Input.GetKey(KeyCode.Space)){
                 if(power <= maxPower){
                     power += 150 * Time.deltaTime;
@@ -38,25 +37,20 @@ public class PlangerScript : MonoBehaviour
             }
 
             if(Input.GetKeyUp(KeyCode.Space)){
-                foreach(Rigidbody r in ballList){
-                    r.AddForce(power * Vector3.forward);
-                }
+                ballRigidbody.AddForce(power * Vector3.forward);
             }
-        } else{
-            ballReady = false;
-            power = 0f;
         }
     }
 
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.CompareTag("Ball")){
-            ballList.Add(other.gameObject.GetComponent<Rigidbody>());
+            ballReady = true;
         }
     }
 
     private void OnTriggerExit(Collider other){
         if(other.gameObject.CompareTag("Ball")){
-            ballList.Remove(other.gameObject.GetComponent<Rigidbody>());
+            ballReady = false;
             power = 0f;
         }
     }
